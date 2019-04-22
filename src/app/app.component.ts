721544701ebee7c400330668f0e86a864bcfb8f4
tleveque <thierry.leveque@askida.com>
2019-04-22 13:05:40 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'editorFocusBug';
+
+  public text: string;
+
+  @ViewChild('AsEditor') asEditor;
+  @ViewChild('editorContainer') editorContainer: ElementRef;
+  public displayAddDialog = false;
+  public newName: string;
+
+
+  onEditorInit() {
+    // We need to replace the tab key normal binding of Quill to be able to use it to move to next/previous field
+    console.log('*** onEditorInit');
+
+    setTimeout(() => {
+        // tslint:disable-next-line:no-console
+        console.debug(`Keyboard binding: ${JSON.stringify(this.asEditor.getQuill().keyboard.bindings['9'])}`);
+        this.asEditor.getQuill().keyboard.bindings['9'] = [{
+          key: 'tab',
+          handler() {
+            // console.debug('Quill: Bind tab to nothing!');
+            return true;
+          }
+        }];
+      },
+      0);
+
+    this.setNoTabIndex();
+  }
+
+  private setNoTabIndex() {
+    if (this.editorContainer) {
+      // tslint:disable-next-line:no-console
+      console.debug(`*** setNoTabIndex`);
+
+      setTimeout( () => {
+        // tslint:disable-next-line:max-line-length
+        const header = (this.editorContainer.nativeElement as HTMLSpanElement).firstElementChild.firstElementChild.firstElementChild.firstElementChild;
+        // console.debug(`*** header is ${header.tagName}`);
+        this.setNoTabIndexForChildren(header.children);
+      }, 0);
+    }
+  }
+
+  private setNoTabIndexForChildren(children: HTMLCollection) {
+    if ( children && children.length > 0 ) {
+      for ( let i = 0; i < children.length; i++) {
+        const child = children.item(i);
+        // console.debug(`Child is of mimeType: ${child.tagName}, class = ${child.className}`);
+        if (child.tagName === 'SPAN') {
+          child.setAttribute('tabindex', '-1');
+        }
+        this.setNoTabIndexForChildren(child.children); // recursive call
+      }
+    }
+  }
+
+  public close() {
+    this.displayAddDialog = false;
+  }
+
+  public open() {
+    this.displayAddDialog = true;
+    setTimeout( () => {
+      document.getElementById('firstInput').focus();
+    }, 100);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule, DialogModule, EditorModule, InputTextModule } from 'primeng/primeng';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    InputTextModule,
+    EditorModule,
+    ButtonModule,
+    DialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
